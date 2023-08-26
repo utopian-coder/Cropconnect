144f9198ad7a1dfba386bc131b66523df2951e18
Manas Baroi <manasbaroi6699@gmail.com>
2023-08-26 20:23:06 +0530

    landing page ui updated

--- client/src/components/Layout/AppLayout.jsx
+++ client/src/components/Layout/AppLayout.jsx
@@ -1,10 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import Testimonial from "./Testimonial";
+import Goals from "../Goals";
 
 function AppLayout() {
   return (
     <main>
-      <h1 className='flex flex-col'>Layout</h1>
+      <h1 className='bg-gray-500'>Layout</h1>
       <Outlet />
+      <Goals />
+      <Testimonial />
+      <Footer />
+      
     </main>
   );
 }
---
+++ client/src/components/Layout/Testimonial.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+
+const Testimonial = () => {
+  return (
+    <div>
+      <div class="container relative flex flex-col justify-between h-full max-w-6xl px-10 mx-auto xl:px-0 mt-5">
+        <h2 class="heading">Testimonials</h2>
+        <p class="mb-12 text-lg text-gray-700">
+          Here is the Testimonial section
+        </p>
+        <div class="w-full">
+          <div class="flex flex-col w-full mb-10 sm:flex-row">
+            <div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+              <div class="relative h-full ml-0 mr-0 sm:mr-10">
+                <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-indigo-500 rounded-lg"></span>
+                <div class="relative h-full p-5 bg-white border-2 border-indigo-500 rounded-lg">
+                  <div class="flex items-center -mt-1">
+                    <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">
+                      Manas Baroi
+                    </h3>
+                  </div>
+
+                  <p class="mb-2 ml-3 text-gray-600">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Officiis temporibus fuga enim ad id, porro assumenda
+                    incidunt hic eius? Reprehenderit esse molestias architecto
+                    animi sed necessitatibus placeat corrupti dolorum? Eos!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="w-full sm:w-1/2">
+              <div class="relative h-full ml-0 md:mr-10">
+                <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-purple-500 rounded-lg"></span>
+                <div class="relative h-full p-5 bg-white border-2 border-purple-500 rounded-lg">
+                  <div class="flex items-center -mt-1">
+                    <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">
+                      Manas Baroi
+                    </h3>
+                  </div>
+
+                  <p class="mb-2 ml-3 text-gray-600">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Dignissimos, voluptas. Iste, quia harum. Natus ad possimus
+                    iure animi quaerat! Deleniti delectus veniam nam velit,
+                    nulla atque. Dolor deleniti at laborum quibusdam culpa,
+                    corporis, nobis fugiat error praesentium ipsum nihil iste.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col w-full mb-5 sm:flex-row">
+            <div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+              <div class="relative h-full ml-0 mr-0 sm:mr-10">
+                <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-blue-400 rounded-lg"></span>
+                <div class="relative h-full p-5 bg-white border-2 border-blue-400 rounded-lg">
+                  <div class="flex items-center -mt-1">
+                    <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">
+                      Pankaj Tripathi
+                    </h3>
+                  </div>
+
+                  <p class="mb-2 ml-3 text-gray-600">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Harum, iusto. Obcaecati iusto iste odio nostrum, assumenda
+                    vel deserunt minus officiis doloribus nesciunt, atque a eum
+                    facilis, aliquam sunt debitis maxime corporis. Sed ratione
+                    rem id minus nisi adipisci impedit in blanditiis iusto,
+                    itaque eius veniam ex dolore ab illo eos?
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+              <div class="relative h-full ml-0 mr-0 sm:mr-10">
+                <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-yellow-400 rounded-lg"></span>
+                <div class="relative h-full p-5 bg-white border-2 border-yellow-400 rounded-lg">
+                  <div class="flex items-center -mt-1">
+                    <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">
+                      Hekar
+                    </h3>
+                  </div>
+
+                  <p class="mb-2 ml-3 text-gray-600">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Autem sunt eius magni facilis quod reiciendis molestiae
+                    ducimus suscipit amet saepe!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="w-full sm:w-1/2">
+              <div class="relative h-full ml-0 md:mr-10">
+                <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-green-500 rounded-lg"></span>
+                <div class="relative h-full p-5 bg-white border-2 border-green-500 rounded-lg">
+                  <div class="flex items-center -mt-1">
+                    <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">
+                      AG
+                    </h3>
+                  </div>
+
+                  <p class="mb-2 ml-3 text-gray-600">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Itaque nesciunt quia, doloribus numquam consectetur
+                    repellendus perspiciatis, beatae dolorem culpa at incidunt
+                    tenetur. assumenda nihil amet
+                    consequatur qui impedit eveniet! Mollitia, molestiae maxime.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
