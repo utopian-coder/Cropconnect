20fe92a7e5fb30fba72d137ab3594853e785624b
Imran Biswas <imranbiswas2209@gmail.com>
2023-08-30 09:22:01 +0530

    Add update profile endpoint.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,9 @@
 const catchAsync = require("../utils/catchAsync");
 
 exports.getUser = catchAsync(async (req, res, next) => {
-  res.json("Ok");
+  const { id } = req.params;
+});
+
+exports.updateUser = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
 });
