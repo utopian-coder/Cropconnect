6c9a3b593834448fa4e8d7eb74354a3f50760a83
Imran Biswas <66299782+utopian-coder@users.noreply.github.com>
2023-12-24 21:47:19 +0530

    Delete client directory

--- client/src/ReduxStore/Reducers/index.jsx
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-import authReducer from "./AuthReducers";
-import postReducer from "./PostReducers";
-// import chatReducer from "./ChatUserReducer";
-
-export const reducers = combineReducers({authReducer,postReducer})
--- client/src/components/Products/ProductList.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Subproduct from "./Subproduct";
-
-export default function ProductList(props) {
-  console.log(props);
-  return props.productlist.map((product, i) => {
-    return <Subproduct product={product} key={i} />;
-  });
-}
